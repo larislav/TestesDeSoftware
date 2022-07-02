4a0185755989b08b9054a4272e42a1d18580d1e3
larislav <laarissa_barros@hotmail.com>
2022-07-02 17:31:12 -0300

    Traits e Fixtures

---
+++ 01 - Testes de Unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    // Objetos fixture são preparados antes de instanciar a classe de teste,
+    // e só é destruído depois que o último teste roda
+
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    {}
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Eduardo",
+                "Pires",
+                DateTime.Now.AddYears(-30),
+                "edu@edu.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
